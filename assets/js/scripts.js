fe6b7d71e19fcfbfbea89bc266d0a36735d8c68d
Radosław Dabrowski <radoslaw.dabrowski@coig.pl>
2019-03-11 14:05:25 +0100

    Modifieed compiled scripts and styles

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,17 @@
-// @prepros-prepend "../libs/theme-1.0.1/multipage/assets/js/jquery.min.js"
-// @prepros-prepend "../libs/theme-1.0.1/multipage/assets/js/jquery.mixitup.min.js"
-// @prepros-prepend "../libs/theme-1.0.1/multipage/assets/js/materialize.min.js"
-// @prepros-prepend "../libs/theme-1.0.1/multipage/assets/js/wow.min.js"
-// @prepros-prepend "../libs/theme-1.0.1/multipage/assets/js/masonry.pkgd.js"
-// @prepros-prepend "../libs/theme-1.0.1/multipage/assets/js/validator.min.js"
+// jQuery
+// @prepros-prepend "../libs/jquery-3.3.1/jquery.min.js"
+// @prepros-prepend "../libs/jquery-3.3.1/jquery.mixitup.min.js"
 
-// @prepros-prepend "../libs/theme-1.0.1/multipage/assets/js/init.js"
+//WOW
+// @prepros-prepend "../libs/scroll-reveal-3.1.4/reveall.min.js"
+
+// Materialize
+// @prepros-prepend "../libs/materialize-1.0.0/js/bin/materialize.min.js"
+
+// Theme
+// @prepros-prepend "../libs/theme-1.0.1/js/masonry.pkgd.js"
+// @prepros-prepend "../libs/theme-1.0.1/js/validator.min.js"
+// @prepros-prepend "../libs/theme-1.0.1/js/init.js"
+
+// Custom
 // @prepros-prepend "_main.js"
