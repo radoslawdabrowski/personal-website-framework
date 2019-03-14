5423794e7373eb42ec3690771336743b887b0bf9
Radosław Dabrowski <radoslaw.dabrowski@coig.pl>
2019-03-14 13:35:01 +0100

    Added data model for education template part

--- cv/admin.py
+++ cv/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import EducationEntity, EducationTypeEntity
+
 # Register your models here.
+admin.site.register(EducationTypeEntity)
+admin.site.register(EducationEntity)
--- cv/models.py
+++ cv/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class EducationTypeEntity(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class EducationEntity(models.Model):
+    symbol = models.TextField(max_length=1)
+    title = models.ForeignKey(EducationTypeEntity, on_delete=models.DO_NOTHING)
+    name = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField(null=True)
+    description = models.TextField(null=True)
