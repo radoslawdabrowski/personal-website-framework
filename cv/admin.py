ddef41d959a221cfa65eccdd95600cf4c2fe87c6
Radosław Dabrowski <kontakt@radoslawdabrowski.pl>
2019-03-14 14:22:04 +0100

    Create models for not categorized attributes of page

--- cv/admin.py
+++ cv/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import EducationEntity, EducationTypeEntity
+from .models import EducationEntity, EducationTypeEntity, AttributeEntity
 
 # Register your models here.
 admin.site.register(EducationTypeEntity)
 admin.site.register(EducationEntity)
+admin.site.register(AttributeEntity)
