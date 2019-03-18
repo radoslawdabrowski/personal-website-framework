4068142c3cffc9d9ed37066559385d72287dff09
Radosław Dabrowski <kontakt@radoslawdabrowski.pl>
2019-03-18 11:06:22 +0100

    Changed project name for production deployment specification

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,6 +4,6 @@ from django.core.wsgi import get_wsgi_application
 
 
 sys.path.append(os.getcwd())
-os.environ['DJANGO_SETTINGS_MODULE'] = "app.settings"
+os.environ['DJANGO_SETTINGS_MODULE'] = "public_python.settings"
 
 application = get_wsgi_application()
