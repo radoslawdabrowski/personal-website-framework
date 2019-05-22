7c9d6446dd622d2a2c2d46c6e95c2f57eacfbcfb
Radosław Dabrowski <kontakt@radoslawdabrowski.pl>
2019-05-22 12:46:49 +0200

    Updated models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+# Contact entity
+class Contact(models.Model):
+    name = models.CharField(default='NO_NAME', max_length=50, error_messages={
+        'max_length': _("Field 'name' is to long")
+    })
+    email = models.CharField(default='NO_EMAIL', max_length=100, error_messages={
+        'max_length': _("Field 'email' is to long")
+    })
+    message = models.TextField(default='NO_MESSAGE')
+
+    def __str__(self):
+        return self.email
+
